bdca758d6974ac0f9b1d3f3a708011b4378399fd
ManoMax <gabrielmaxcontato@gmail.com>
2019-10-27 11:46:33 -0300

    Adicao de projeto

---
+++ ajude/src/main/java/v1/ajude/AjudeApplication.java
@@ -0,0 +1,13 @@
+package v1.ajude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjudeApplication.class, args);
+	}
+
+}
---
+++ ajude/src/test/java/v1/ajude/AjudeApplicationTests.java
@@ -0,0 +1,13 @@
+package v1.ajude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
