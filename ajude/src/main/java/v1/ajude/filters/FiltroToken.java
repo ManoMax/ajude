cb00bd762b5eb2d1f0c21b696e3c373a5141ec40
ManoMax <gabrielmaxcontato@gmail.com>
2019-10-28 16:45:02 -0300

    Criação de Filtro Token (problema de importação io)

---
+++ ajude/src/main/java/v1/ajude/filters/FiltroToken.java
@@ -0,0 +1,46 @@
+package v1.ajude.filters;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureException;
+//import io.jsonwebtoken.ExpiredJwtException;
+//import io.jsonwebtoken.MalformedJwtException;
+//import io.jsonwebtoken.PrematureJwtException;
+//import io.jsonwebtoken.UnsupportedJwtException;
+
+public class FiltroToken extends GenericFilterBean {
+    public final static int TOKEN_INDEX = 7;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        String header = req.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            throw new ServletException("Token inexistente ou mal formatado!");
+        }
+
+        // Extraindo apenas o token do cabecalho.
+        String token = header.substring(TOKEN_INDEX);
+
+//        try {
+//            Jwts.parser().setSigningKey("login do ajude").parseClaimsJws(token).getBody();
+//        } catch(SignatureException | ExpiredJwtException | MalformedJwtException | PrematureJwtException | UnsupportedJwtException | IllegalArgumentException e) {
+//            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+//            return;
+//        }
+
+        chain.doFilter(request, response);
+    }
+}
