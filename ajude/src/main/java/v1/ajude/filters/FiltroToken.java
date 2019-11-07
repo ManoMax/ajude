73bd341dd074cb20548654e53f69efc5b4b1caed
ManoMax <gabrielmaxcontato@gmail.com>
2019-11-07 11:01:45 -0300

    add @Bean CORSFilter

--- ajude/src/main/java/v1/ajude/filters/FiltroToken.java
+++ ajude/src/main/java/v1/ajude/filters/FiltroToken.java
@@ -17,6 +17,12 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.PrematureJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 public class FiltroToken extends GenericFilterBean {
     public final static int TOKEN_INDEX = 7;
 
@@ -43,5 +49,16 @@ public class FiltroToken extends GenericFilterBean {
         }
 
         chain.doFilter(request, response);
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 }
