5dfd71e74d02b66c72494209a24782825d4f4974
ManoMax <gabrielmaxcontato@gmail.com>
2019-11-07 12:06:59 -0300

    Atualizando Bean COAR

--- src/main/java/v1/ajude/AjudeApplication.java
+++ src/main/java/v1/ajude/AjudeApplication.java
@@ -3,11 +3,28 @@ package v1.ajude;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 @SpringBootApplication
 public class AjudeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AjudeApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
 	}
 
 }
