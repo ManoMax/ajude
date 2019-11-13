5ff3fb7ecba8dd3fcb12cf0f153a0372293bb169
ManoMax <gabrielmaxcontato@gmail.com>
2019-11-13 16:03:38 -0300

    Respostas para Comentarios

--- src/main/java/v1/ajude/models/Comentario.java
+++ src/main/java/v1/ajude/models/Comentario.java
@@ -1,4 +1,28 @@
 package v1.ajude.models;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.ArrayList;
+
 public class Comentario {
+
+    private Usuario usuario;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idComent;
+    private String comentario;
+    // private ArrayList<Respostas> respostas;
+
+    public Comentario() {
+        super();
+    }
+
+    public Comentario(Usuario usuario, String comentario) {
+        this.usuario = usuario;
+        this.comentario = comentario;
+    }
+
+
 }
